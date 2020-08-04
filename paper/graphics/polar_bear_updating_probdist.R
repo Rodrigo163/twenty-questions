ab6cf7700162da20838a9d207a6c2e8f25e991fa
Elizabeth Pankratz <ecpankratz@gmail.com>
2020-08-04 12:04:36 +0200

    Start writing paper

--- paper/graphics/polar_bear_updating_probdist.R
+++ paper/graphics/polar_bear_updating_probdist.R
@@ -6,7 +6,19 @@ library(dplyr)
 library(tidyr)
 library(purrr)
 library(extrafont)
+library(reshape2)
 loadfonts()
 
 data <- read.csv(file="polar_bear_updating_probdist.csv", header=TRUE, sep=",")
 
+data_long <- melt(data)
+names(data_long) <- c('animal', 'question', 'value')
+
+
+
+ggplot(data_long, aes(x=animal, y=value)) +
+  geom_bar(stat='identity') +
+  facet_wrap( ~ question , ncol=1) +
+  NULL
+  
+# TODO: rotate x axis labels
