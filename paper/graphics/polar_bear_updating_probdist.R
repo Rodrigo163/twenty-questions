5753356d9a980a2fa30d9b5744d3151dee7762ff
Elizabeth Pankratz <ecpankratz@gmail.com>
2020-08-04 12:49:29 +0200

    Update paper and add updating graphic

--- paper/graphics/polar_bear_updating_probdist.R
+++ paper/graphics/polar_bear_updating_probdist.R
@@ -9,7 +9,7 @@ library(extrafont)
 library(reshape2)
 loadfonts()
 
-data <- read.csv(file="polar_bear_updating_probdist.csv", header=TRUE, sep=",")
+data <- read.csv(file="polar_bear_updating_probdist_subset.csv", header=TRUE, sep=",")
 
 data_long <- melt(data)
 names(data_long) <- c('animal', 'question', 'value')
@@ -18,7 +18,10 @@ names(data_long) <- c('animal', 'question', 'value')
 
 ggplot(data_long, aes(x=animal, y=value)) +
   geom_bar(stat='identity') +
-  facet_wrap( ~ question , ncol=1) +
+  facet_wrap( ~ question , ncol=1) + 
+  theme(axis.text.x = element_text(angle = 90, hjust=1, vjust=0.3),
+        text = element_text(family='Times New Roman')) +
+  labs(y=element_blank(), x=element_blank()) +
   NULL
   
-# TODO: rotate x axis labels
+
